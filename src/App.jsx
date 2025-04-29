5d941dcca546c2e697b25286cab3f068a6196766
SuDenDimon <sgp7766@gmail.com>
2025-04-29 23:00:54 +0300

    task1 start

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,26 @@ import Heading from './components/Heading/Heading';
 import Section from './components/Section/Section';
 import Statistics from './components/Statistics/Statistics';
 
+import article from './data/article';
+
 export const App = () => {
   return (
     <Section>
       <Container>
         <Heading title="Task 1 Blog Card" bottom />
-        <BlogCard />
-
+        <BlogCard
+          poster={article.poster}
+          tag={article.tag}
+          title={article.title}
+          description={article.description}
+          userName={article.name}
+          avatar={article.avatar}
+          postedAt={article.postedAt}
+        />
         <Heading title="Task 2 Statistics" top bottom />
         <Statistics title="Main Statistics" />
-
         <Heading title="Task 3 Forbes list" top bottom />
         <ForbesList />
-
         <Heading title="Task 4 Crypto history" top bottom />
         <CryptoHistory />
       </Container>
--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,4 +1,48 @@
-const BlogCard = () => {
-  return <h2>BlogCard</h2>;
+import styles from './BlogCard.module.css';
+const BlogCard = ({
+  poster,
+  tag,
+  title,
+  description,
+  userName,
+  avatar,
+  postedAt,
+}) => {
+  return (
+    <>
+      <h2>BlogCard</h2>
+      <div className={styles.card}>
+        <div className={styles.cardHeader}>
+          <img
+            className={styles.cardPoster}
+            src="<https://source.unsplash.com/600x400/?computer>"
+            alt="card__image"
+          />
+        </div>
+        <div className={styles.cardBody}>
+          <span className={styles.tag}>Technology</span>
+          <h2 className={styles.cardTitle}>What's new in 2022 Tech</h2>
+          <p className={styles.cardText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+            perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea
+            atque quidem!
+          </p>
+        </div>
+        <div className={styles.cardFooter}>
+          <div className={styles.userBox}>
+            <img
+              className={styles.avatar}
+              src="<https://i.pravatar.cc/40?img=1>"
+              alt="Jane Doe"
+            />
+            <div>
+              <h3 className={styles.userName}>Jane Doe</h3>
+              <small className={styles.date}>2h ago</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default BlogCard;
